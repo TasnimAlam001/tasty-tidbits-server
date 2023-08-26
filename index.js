c8759ccfd5404313e1d8b2a02d069aa0935fd313
tasnimalam001 <tasnimalam.001@gmail.com>
2023-08-27 01:28:05 +0600

    usersCollection

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zlgqora.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,10 +26,21 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const usersCollection = client.db("tidbits").collection("users");
     const menuCollection = client.db("tidbits").collection("menu");
     const reviewCollection = client.db("tidbits").collection("review");
     const cartCollection = client.db("tidbits").collection("carts");
 
+    // users collection
+    app.post('/users', async(req, res)=>{
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    })
+
+    
+
+    //menu collection apis
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
@@ -45,19 +56,37 @@ async function run() {
 
     //Cart Collection
 
+    app.get('/carts', async(req, res)=>{
+      const email = req.query.email;
+      if(!email){
+        res.send('pai nai')
+      }
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+
+
+    })
+
+
     app.post("/carts", async (req, res) => {
       try {
         const item = req.body;
-        console.log(item);
         const result = await cartCollection.insertOne(item);
         res.send(result);
       } catch (error) {
-        console.error(error);
         res
           .status(500)
           .json({ error: "An error occurred while adding to the cart." });
       }
     });
+
+    app.delete('/carts/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    })
 
 
 
