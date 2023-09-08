49a2b98c250aba1f8913fe68b60ee4c1254b924a
tasnimalam001 <tasnimalam.001@gmail.com>
2023-09-09 00:07:08 +0600

    jwt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -8,6 +9,33 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req,res,next)=>{
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error: true, message: 'unauthorized access'});
+  }
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+    if(err){
+      return res.status(401).send({error: true, message: 'unauthorized access'})
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zlgqora.mongodb.net/?retryWrites=true&w=majority`;
@@ -31,14 +59,47 @@ async function run() {
     const reviewCollection = client.db("tidbits").collection("review");
     const cartCollection = client.db("tidbits").collection("carts");
 
-    // users collection
-    app.post('/users', async(req, res)=>{
+    app.post("/jwt", (req, res) => {
       const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h"
+      });
+      res.send({ token });
+    });
+
+
+
+
+
+    // users collection
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user Already Exist" });
+      }
       const result = await usersCollection.insertOne(user);
       res.send(result);
-    })
+    });
 
-    
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateRole = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateRole);
+      res.send(result);
+    });
 
     //menu collection apis
     app.get("/menu", async (req, res) => {
@@ -52,22 +113,23 @@ async function run() {
 
 
 
-
-
     //Cart Collection
 
-    app.get('/carts', async(req, res)=>{
+    app.get("/carts", verifyJWT, async (req, res) => {
       const email = req.query.email;
-      if(!email){
-        res.send('pai nai')
+      if (!email) {
+        res.send("pai nai");
       }
+
+      const decodedEmail = req.decoded.email;
+      if(email !== decodedEmail){
+        return res.status(403).send({error: true, message: 'Forbidden access'});
+      }
+
       const query = { email: email };
       const result = await cartCollection.find(query).toArray();
       res.send(result);
-
-
-    })
-
+    });
 
     app.post("/carts", async (req, res) => {
       try {
@@ -81,17 +143,13 @@ async function run() {
       }
     });
 
-    app.delete('/carts/:id', async(req, res)=>{
+    app.delete("/carts/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await cartCollection.deleteOne(query);
       res.send(result);
-    })
+    });
 
-
-
-
-    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -110,3 +168,6 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`tasty Tidbits is running on port ${port}`);
 });
+
+
+// for generate random access Token : require('crypto').randomBytes(64).toString('hex')
